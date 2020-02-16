e911258f320adea2488473f7f369fd666528de1e
Marina Poprotskaya <mitsevich.marina@gmail.com>
2020-02-16 18:06:36 +0300

    docs: add exercise

--- lesson_5/js/script.js
+++ lesson_5/js/script.js
@@ -99,3 +99,76 @@ function getEven(arr) {
  } ;
 getEven([2,4,6,8,0,9,3]);
 console.log(arrEven);
+
+
+//Упражнение 9
+// Дано число. Сложите его цифры. Если сумма получилась более 9-ти, опять сложите его
+// цифры. И так, пока сумма не станет однозначным числом (9 и менее). Исп. рекурсию.
+
+function sumNumber(num) {
+num = String(num);
+let sum = 0;
+console.log(num + ': ' + typeof num);
+
+for  (let i=0; i<num.length; i++) {
+    //console.log(num[i] +': '+ typeof num[i]);
+
+    sum+=+num[i];
+}
+console.log(sum);
+
+if (sum > 9) {
+    sumNumber(sum);
+}
+}
+
+sumNumber(4653);
+// sumNumber(prompt('Введите число'));
+
+
+
+//Упражнение 1, ДЗ_5
+
+// 1. Напишите ф-цию, запрашивающую имя, фамилия, отчество и номер группы студента и
+// выводящую введённые данные в следующем виде:
+// *****************************
+// * Домашняя работа: «Функции» *
+// * Выполнил: студент гр. W4017 *
+// * Иванов Иван Иванович *
+// *****************************
+// Размер рамки должен определятся автоматически по самой длинной строке. Рамку
+// вывести в консоль.
+
+function card() {
+let name = prompt('Введите имя', '');
+let lastName = prompt('Введите фамилию', '');
+let secondName = prompt('Введите фамилию', '');
+let group = prompt('Введите группу', '');
+strArr=[];
+    
+strArr.push('Домашняя работа: «Функции»');
+strArr.push('Выполнил: студент гр.' + group);
+strArr.push(lastName + ' ' + name + ' ' + secondName);
+
+console.log(strArr);
+
+// let maxLength = Math.max(strArr[0].length,strArr[1].length,strArr[2].length);   ----Можно и так
+
+let maxLength = 0;
+for (let i = 0; i < strArr.length; i++) {
+    if(strArr[i].length > maxLength) {
+        maxLength = strArr[i].length;
+    }
+}
+console.log(strArr);
+console.log(maxLength);
+
+for (let i = 1; i <= strArr.length + 2; i++) {
+    if (i==1 || i == strArr.length+2) {
+        console.log('*'.repeat(maxLength+4));
+    } else {
+        console.log('* '+ strArr[i-2] + ' '.repeat(maxLength-strArr[i-2].length) + ' *');
+    }
+}
+}
+card();
